5428ebbb2122e4220db047224947c86e44fcdd9c
Himawari G <github@gsolo.com>
2023-04-19 16:23:36 -0400

    Update webui.cmd

--- webui.cmd
+++ webui.cmd
@@ -1,5 +1,6 @@
 :: Digil TextGen
 call C:\ProgramData\Miniconda3\Scripts\activate.bat	 
 call conda activate textgen
+call pip install -r requirements.txt --upgrade
 call python server.py --load-in-8bit
 pause
