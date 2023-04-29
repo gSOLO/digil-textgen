3f6de3a80fbb8b4fcff8d422cb3b6214a0794adf
Himawari G <github@gsolo.com>
2023-04-29 16:02:18 -0400

    Create Upgrade Command

--- upgrade.cmd
+++ upgrade.cmd
@@ -1,5 +1,5 @@
 :: Digil TextGen
 call C:\ProgramData\Miniconda3\Scripts\activate.bat	 
 call conda activate textgen
-call python server.py --chat --model vicuna-13b-GPTQ-4bit-128g --wbits 4 --groupsize 128
+call pip install -r requirements.txt --upgrade
 pause
--- webui.cmd
+++ webui.cmd
@@ -1,6 +1,5 @@
 :: Digil TextGen
 call C:\ProgramData\Miniconda3\Scripts\activate.bat	 
 call conda activate textgen
-call pip install -r requirements.txt --upgrade
 call python server.py --load-in-8bit
 pause
